c805978275f73bd404c3c1ede3fdea20314ee9a4
Igormandello <igormandello@gmail.com>
2018-10-09 09:08:52 -0300

    Creates the RotateAround script and applies it to the Moon

---
+++ solar-system/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public GameObject target;
+
+    [Range(0.1f, 5)]
+    public float speed = 0.1f;
+
+    void Start()
+    { }
+
+
+	void Update ()
+    {
+        this.transform.RotateAround(this.target.transform.position, Vector3.up, this.speed);
+	}
+}
