76ceb41644457721309f17f4f38b126514165ade
Igormandello <igormandello@gmail.com>
2018-10-10 11:52:10 -0300

    Adds the 79 jupiter moons

--- solar-system/Assets/Scripts/PlanetManager.cs
+++ solar-system/Assets/Scripts/PlanetManager.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
+    public GameObject jupiterMoon;
+
     [Range(0.1f, 3)]
     public const int scale = 1;
 
     private readonly Dictionary<string, float> speed = new Dictionary<string, float>
     {
+        { "Sun", 2 },
         { "Mercury", 3 },
         { "Venus", 2 },
         { "Earth", 1.8f },
@@ -38,7 +41,24 @@ public class PlanetManager : MonoBehaviour
             controller.OnPlanetClick += FocusCamera;
             controller.speed = speed[planets[i].name] * scale;
         }
-	}
+
+        PlanetController sunController = sun.AddComponent<PlanetController>();
+        sunController.OnPlanetClick += FocusCamera;
+        sunController.speed = speed["Sun"] * scale;
+
+        GameObject jupiter = GameObject.Find("Jupiter");
+        for (int n = 0; n < 79; n++)
+        {
+            GameObject g = Instantiate(jupiterMoon);
+            g.transform.parent = jupiter.transform;
+            g.transform.localScale = new Vector3(0.05f, 0.05f, 0.05f);
+            g.transform.localPosition = new Vector3(Random.Range(-1, 1), Random.Range(-0.6f, 0.6f), Random.Range(-1, 1));
+
+            RotateAround rotateObj = g.AddComponent<RotateAround>();
+            rotateObj.target = jupiter;
+            rotateObj.speed = Random.Range(0.5f, 1.5f);
+        }
+    }
 
     private void Update()
     {
